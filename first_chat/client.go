390c27ce0aaaf267075c0676787e0857758f8155
delvintang <delvintang@tencent.com>
2020-05-02 15:10:08 +0800

    feat: add some annotation

--- first_chat/client.go
+++ first_chat/client.go
@@ -6,17 +6,20 @@ import (
 )
 
 func main() {
+	// 第一步，建立连接
 	conn, err := net.Dial("tcp", "127.0.0.1:8000")
 	defer conn.Close()
 	if err != nil {
 		panic(err)
 	}
 
+	// 第二步，发送请求包
 	if _, err := conn.Write([]byte("hello")); err != nil {
 		fmt.Println("conn Write error,", err)
 		return
 	}
 
+	// 第三步，接收 server 回包
 	buffer := make([]byte, 1024)
 	recvNum, err := conn.Read(buffer)
 	if err != nil {
--- first_chat/server.go
+++ first_chat/server.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
+	// 第一步，监听请求
 	lis, err := net.Listen("tcp","127.0.0.1:8000")
 	if err != nil {
 		panic(err)
 	}
 
 	for {
+		// 第二步，获取一个连接
 		conn, err := lis.Accept()
 		defer conn.Close()
 
@@ -20,6 +22,7 @@ func main() {
 			continue
 		}
 
+		// 第三步，读取 client 请求包
 		buffer := make([]byte, 1024)
 		recvNum, err := conn.Read(buffer)
 		if err != nil {
@@ -27,6 +30,7 @@ func main() {
 			continue
 		}
 
+		// 第四步，发送响应包
 		msg := string(buffer[:recvNum])
 		fmt.Println("recv msg : ", msg)
 	}
